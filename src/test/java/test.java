dea8e72211aac115555d860a6bf59730df6a3746
Jonathan.Carrasco <jonathan.carrasco@astontech.com>
2023-06-20 15:54:54 -0500

    added more test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -98,7 +98,7 @@ public class test {
 
     //endregion
 
-    //region
+    //regionTest
 
 
     //using Arrays to check if its equal
@@ -253,8 +253,6 @@ public class test {
 
 
 
-
-
     }
 
 
@@ -278,19 +276,46 @@ public class test {
 
 
     }
-    //endregion
 
+
+
+    @Test
+    public int climbStairs(int n) {
+
+        //given the number, we would check to see how many times we could add 1 or 2 together to get that number
+        if(n == 1){
+            return 1;
+        }
+
+        int dp[] = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n ; i++) {
+            dp[i] = dp[ i- 1] + dp[i -2];
+        }
+
+        return dp[n];
+
+
+    }
+
+
+
+
+
+
+    //endregion
 
     //regionDATA STRUCTURES COURSE
 
-
-    //stacks- LIFO- last-in first-out
-    //stores objects into a sort of "vertical tower"
-    //push() to add to te top
-    //pop() to remove from the top
-
     @Test
     public void testStack(){
+        //stacks- LIFO- last-in first-out
+        //stores objects into a sort of "vertical tower"
+        //push() to add to te top
+        //pop() to remove from the top
+
         Stack<String> stack = new Stack<String>();
 
         //adds to the stack
@@ -309,9 +334,228 @@ public class test {
         //search inside stack
 //        System.out.println(stack.search("MineCraft"));
 
+        //loop through, can run out of memory
+        for (int i = 0; i < 1000000; i++) {
+            stack.push("SomeGame");
+        }
+
+    }
 
 
 
+    @Test
+    public void queueTest(){
+
+        /*
+        Queue = FIFO = First-in First-Out
+        collection designed for holding elements prior to processing
+        Linear Data structure
+        add = enqueue/ add object to tail ,offer()
+        remove = dequeue / remove object from head, poll()
+        extended collections class.
+
+        a interface
+         */
+
+
+        Queue<String> queue = new LinkedList<String>();
+
+        //add
+        queue.offer("Karen");
+        queue.offer("Chad");
+        queue.offer("Steve");
+        queue.offer("Jason");
+
+        ///check to see if queue is empty
+//        System.out.println(queue.isEmpty());
+
+        //check size of queue
+        System.out.println(queue.size());
+
+        //check first entry
+//        System.out.println(queue.peek());
+
+        //check to see if queue has certain object
+//        System.out.println(queue.contains("Jason"));
+
+        //remove first head
+//        queue.poll();
+
+        System.out.println(queue);
+
+        /*
+        Where Can Queues Be useful
+
+        Keyboard Buffers(letters should appeared on the screen in the order they're passed
+        Printer Queue(Print jobs should be completed inorder
+        Used in LinkedList, PriorityQueues, Breadth-first-search
+         */
+
+
+    }
+
+    @Test
+    public void testPriorityQ(){
+        // Priority Queue = FIFO  with highest priorities first before elements with low priority
+
+        //to reverse order use Collections.reverseOrder()
+        Queue<String> queue = new PriorityQueue<>();
+
+        queue.offer("b");
+        queue.offer("a");
+        queue.offer("c");
+        queue.offer("d");
+        queue.offer("f");
+
+        while(!queue.isEmpty()){
+            System.out.println(queue.poll());
+        }
+
+
+    }
+
+
+    @Test
+    public void linkedListTest(){
+
+        /*
+        LinkedList = stores nodes in 2 parts (data + address)
+        nodes are in non-consecutive memory location
+        element are linked using pointers
+
+        Pros:
+        Dynamic data structures
+        Insertion and deletion of nodes is easy
+        no/low memory waste
+
+        Cons:
+        Greater memory usage
+        no random access of element
+        accessing/ searching element is more time consuming
+
+        uses:
+        implement Stacks/Queues
+        GPS navigation
+        music playlist
+
+         */
+
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        /*
+        linkedList.push("A");
+        linkedList.push("B");
+        linkedList.push("C");
+        linkedList.push("D");
+        linkedList.push("F");
+        linkedList.pop();
+         */
+
+        //LinkedList as Queue
+        linkedList.offer("A");
+        linkedList.offer("B");
+        linkedList.offer("C");
+        linkedList.offer("D");
+        linkedList.offer("F");
+//        linkedList.poll();
+
+        //add to list
+        linkedList.add(4, "E");
+
+        //remove from list
+        linkedList.remove("E");
+
+        //search
+        System.out.println(linkedList.indexOf("F"));
+
+        //peak at head or tail
+        System.out.println(linkedList.peekFirst());
+        System.out.println(linkedList.peekLast());
+
+        //add to the head
+        linkedList.addFirst("0");
+
+        //add to the tail
+        linkedList.addLast("G");
+
+        String first = linkedList.removeFirst();
+
+        String last = linkedList.removeLast();
+
+
+        System.out.println(linkedList);
+
+
+    }
+
+    @Test
+    public void dynamicArrayTest(){
+
+        /*
+        PROS:
+        random access of elements 0(1)
+        good locality of reference and data cache utilization
+        easy to insert / delete at the end
+
+        CONS:
+        waste more memory
+        shifting elements is time consuming 0(n)
+        expanding/ shrinking the array is time consuming
+
+         */
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        System.out.println();
+
+
+
+    }
+
+
+    @Test
+    public void LinkedListArrayListTest(){
+
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        long startTime;
+        long endtime;
+        long elapsedTime;
+
+        for (int i = 0; i < 100000; i++) {
+            linkedList.add(i);
+            arrayList.add(i);
+        }
+
+
+        //Linked List
+        startTime = System.nanoTime();
+
+        linkedList.get(0);
+
+        //do something;
+
+        endtime = System.nanoTime();
+
+        elapsedTime = endtime - startTime;
+
+        System.out.println("LinkedList:\t" + elapsedTime+ " ns" );
+
+
+
+        //ArrayList
+        startTime = System.nanoTime();
+
+        arrayList.get(0);
+
+        //do something;
+
+        endtime = System.nanoTime();
+
+        elapsedTime = endtime - startTime;
+
+        System.out.println("ArrayList:\t" + elapsedTime+ " ns" );
 
     }
 
